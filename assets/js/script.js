534b23e264050b8e5cb75d3836feed25c29a3946
unknown <elijahsatten@hotmail.com>
2023-12-14 21:01:02 -0500

    changed api key link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var searchButton = $('#search-btn');
 var searchInput = $('#searchInput');
 var recentHistory = $('#recentHistory');
-var cardRow = $('#cardRow')
-var cityName = 'undefined';
+var cardRow = $('#cardRow');
+var cityName = '';
 var lat = 0;
 var lon = 0;
 var temp = [];
@@ -29,40 +29,34 @@ function searchButtonHandler() {
 
 function convertGeo() {
     var inputString = searchInput.val().toLowerCase();
-    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputString + '&limit=1&appid=e9b8186425312aeb2aeb30949d61fa5c';
+    var requestURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + inputString + '&limit=1&appid=074a9a3f7e74b09b7c2d304b506e1f48';
 
     fetch(requestURL)
     .then(response => {          
         return response.json();
     })
     .then(data => {
-        cityName = data[0].name;
-        lat = data[0].lat;
-        lon = data[0].lon;
+        cityName = data[0];
+        lat = data[0];
+        lon = data[0];
         getAPI();
     });
 }
 
 function getAPI() {
-    var requestForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=e9b8186425312aeb2aeb30949d61fa5c&units=imperial'
+    var requestForecast = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=074a9a3f7e74b09b7c2d304b506e1f48&units=imperial'
     fetch(requestForecast)
     .then(response => {          
         return response.json();
     })
     .then(data => {
-        for (var i = 0; i < data.list.length; i += 8) {
+        for (var i = 0; i < data.list; i += 8) {
             temp.push(data.list[i].main.temp);
             wind.push(data.list[i].wind.speed);
             humidity.push(data.list[i].main.humidity);
             date.push(data.list[i].dt_txt);
 
         }
-        renderAPI();
     });
 }
-
-function renderAPI() {
-    
-}
-
 searchButton.on('click', searchButtonHandler);
